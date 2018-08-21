6848c61ad4c6299909c528dcc05e1b4771bda713
Danny Phillips <dannyp@procore.com>
2018-08-21 11:02:55 -0700

    first commit

---
+++ core_install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+clear
+
+echo "============================================================"
+echo "Setting Up The Things"
+echo "============================================================"
+
+echo "============================================================"
+echo "INSTALLING HOMEBREW"
+echo "============================================================"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+
+echo "============================================================"
+echo "INSTALLING GIT"
+echo "============================================================"
+brew install git
+echo "GIT GLOBAL USER NAME"
+read user_name
+git config --global user.name $user_name
+echo "GIT GLOBAL EMAIL"
+read user_email
+git config --global user.email $user_email
+
+echo "============================================================"
+echo "INSTALLING ZSH"
+echo "============================================================"
+brew install zsh
+
+echo "============================================================"
+echo "INSTALLING OH-MY-ZSH"
+echo "============================================================"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "============================================================"
+echo "CLONING DANNYPHILLIPS/CFG"
+echo "============================================================"
+git clone https://github.com/dannyphillips/cfg.git ~/CodeProjectsc/cfg
+mkdir ~/.oh-my-zsh/custom/themes
+ln -sf ~/CodeProjectsc/cfg/.oh-my-zsh/custom/themes/mein.zsh-theme ~/.oh-my-zsh/custom/themes/mein.zsh-theme
+ln -sf ~/CodeProjectsc/cfg/.zshrc
+
+echo "============================================================"
+echo "INSTALLING SILVER SEARCHER"
+echo "============================================================"
+brew install the_silver_searcher
+
+echo "============================================================"
+echo "INSTALLING FZF"
+echo "============================================================"
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+
+echo "============================================================"
+echo "INSTALLING PYTHON3"
+echo "============================================================"
+brew install python3
+
+echo "============================================================"
+echo "INSTALLING NEOVIM"
+echo "============================================================"
+brew install neovim
+pip3 install neovim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir .config
+mkdir .config/nvim
+ln -sf ~/CodeProjectsc/cfg/.config/nvim/init.vim .config/nvim/init.vim
+
+echo "============================================================"
+echo "INSTALLING CTAGS"
+echo "============================================================"
+brew install ctags
+ln -sf ~/CodeProjectsc/cfg/.ctags
+
+echo "============================================================"
+echo "SYMLINKING ITERM2"
+echo "============================================================"
+ln -s ~/CodeProjectsc/cfg/iterm/com.googlecode.iterm2.plist ~/Library/Preferences
+
+echo "============================================================"
+echo "INSTALLING POWERLINE FONTS"
+echo "============================================================"
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
---
+++ mac_install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+
+echo "============================================================"
+echo "INSTALLING RBENV"
+echo "============================================================"
+brew install rbenv ruby-build
+
+echo "============================================================"
+echo "INSTALLING RUBY"
+echo "============================================================"
+rbenv install 2.4.2 --verbose
+rbenv global 2.4.2
+source ~/.zshrc
+rbenv rehash
+gem install bundler
+gem install pry
+
+echo "============================================================"
+echo "INSTALLING POSTGRES"
+echo "============================================================"
+brew install postgresql
+brew services start postgresql
+createuser -s
+createdb
+
+echo "============================================================"
+echo "INSTALLING NVM"
+echo "============================================================"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+source ~/.zshrc
+nvm install node
+nvm use node
+npm install -g yarn
+source ~/.zshrc
